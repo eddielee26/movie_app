0ea332c84cdf37db44696f83b672b867ef527120
eddie <eddielee001@gmail.com>
2019-09-09 17:17:52 -0500

    adds actor and movie associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   # validates_format_of :title, with: /[a-zA-Z0-9]/ # validation for english only
-  validates :title, :year, :plot, :director, presence: true
-  validates :plot, length: { in: 1..500 }
+  # validates :title, :year, :plot, :director, presence: true
+  # validates :plot, length: { in: 1..500 }
+  has_many :actors
 end

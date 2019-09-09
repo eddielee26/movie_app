69ec65185b3104dff79b23bcbff4c23248c4047f
eddie <eddielee001@gmail.com>
2019-09-08 21:21:37 -0500

    adds validations for actor/movie models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ApplicationRecord
   # validates_format_of :title, with: /[a-zA-Z0-9]/ # validation for english only
+  validates :title, :year, :plot, :director, presence: true
+  validates :plot, length: { in: 1..500 }
 end

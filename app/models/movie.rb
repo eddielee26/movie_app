9fb5511dc06c8403bb96255745fc3b91e5454475
eddie <eddielee001@gmail.com>
2019-09-15 13:24:36 -0500

    adds has many through association

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,10 @@ class Movie < ApplicationRecord
   # validates :title, :year, :plot, :director, presence: true
   # validates :plot, length: { in: 1..500 }
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.map { |genre| genre.name }
+  end  
 end
